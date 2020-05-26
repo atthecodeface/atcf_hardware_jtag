f8eea123f543cac4fb1d4e599b55628559849008
Gavin Stark <gjstark@me.com>
2020-05-26 11:46:33 +0100

    [header] Added APB target to header

--- cdl/jtag_modules.h
+++ cdl/jtag_modules.h
@@ -1,5 +1,30 @@
+/** Copyright (C) 2016-2020,  Gavin J Stark.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file   jtag_modules.h
+ * @brief  Modules for JTAG TAP and APB access
+ *
+ * Header file for the modules for JTAG
+ *
+ */
+
+/*a Includes */
 include "apb::apb.h"
 include "jtag.h"
+
+/*a Modules */
+/*m jtag_tap_apb */
 extern module jtag_tap( clock jtag_tck,
                         input bit reset_n,
                         input t_jtag jtag,
@@ -16,6 +41,7 @@ extern module jtag_tap( clock jtag_tck,
     timing from rising clock jtag_tck tdo, ir, dr_action, dr_in;
 }
 
+/*m jtag_tap_apb */
 extern module jtag_tap_apb( clock jtag_tck,
                  input bit reset_n,
 
@@ -37,3 +63,23 @@ extern module jtag_tap_apb( clock jtag_tck,
     timing comb input dr_in, dr_action, ir;
     timing comb output dr_out, dr_tdi_mask;
 }
+
+/*m apb_target_jtag */
+extern module apb_target_jtag( clock clk         "System clock",
+                               input bit reset_n "Active low reset",
+
+                               input  t_apb_request  apb_request  "APB request",
+                               output t_apb_response apb_response "APB response",
+
+                               output bit             jtag_tck_enable,
+                               output t_jtag          jtag,
+                               input  bit             jtag_tdo
+
+    )
+{
+    timing to   rising clock clk apb_request;
+    timing from rising clock clk apb_response;
+    timing from rising clock clk jtag_tck_enable, jtag;
+    timing to   rising clock clk jtag_tdo;
+}
+
