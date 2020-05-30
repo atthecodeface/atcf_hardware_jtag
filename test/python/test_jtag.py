eb7425349bf75ffb50fc4cb49f05a34cef6c9fee
Gavin Stark <gjstark@me.com>
2020-05-30 13:26:58 +0100

    [python] Removed line from comment

--- test/python/test_jtag.py
+++ test/python/test_jtag.py
@@ -1,7 +1,6 @@
 #a Copyright
 #  
 #  This file 'test_jtag.py' copyright Gavin J Stark 2017
-#  
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
